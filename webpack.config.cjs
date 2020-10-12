7fa9e38cdc89005c60badf47a97ee0b65672ce1a
Johnson Jan Su <su1221jan@gmail.com>
2020-10-12 11:34:24 +0800

    update webpack config

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -40,183 +40,175 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const { InjectManifest } = require('workbox-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-    .BundleAnalyzerPlugin
+	.BundleAnalyzerPlugin
 const TerserPlugin = require('terser-webpack-plugin')
 const webpack = require('webpack')
 const dotenv = require('dotenv')
 const path = require('path')
 const env = dotenv.config().parsed
 const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[next] = JSON.stringify(env[next])
-    return prev
+	prev[next] = JSON.stringify(env[next])
+	return prev
 }, {})
 
 module.exports = {
-    entry: './client/index.js',
-    mode: env.NODE_ENV,
-    // devtool: 'none',
-    output: {
-        path: path.join(__dirname, 'server/dist'),
-        filename: './js/[name].bundle.js',
-        chunkFilename: './js/[name].bundle.js',
-        publicPath: '/',
-    },
+	entry: './client/index.js',
+	mode: env.NODE_ENV,
+	// devtool: 'none',
+	output: {
+		path: path.join(__dirname, 'server/dist'),
+		filename: './js/[name].bundle.js',
+		chunkFilename: './js/[name].bundle.js',
+		publicPath: '/',
+	},
 
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.(eot|woff|woff2|[ot]tf)$/,
-                use: {
-                    loader: 'file-loader?limit=100000&name=[name].[ext]',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'fonts',
-                    },
-                },
-            },
-            {
-                test: /\.(webp|svg|png|jpe?g|gif)(\?\S*)?$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'imgs',
-                        },
-                    },
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            bypassOnDebug: true,
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: ['sass-loader'],
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-            },
-        ],
-    },
-    devServer: {
-        historyApiFallback: true,
-    },
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+					},
+				],
+			},
+			{
+				test: /\.(eot|woff|woff2|[ot]tf)$/,
+				use: {
+					loader: 'file-loader?limit=100000&name=[name].[ext]',
+					options: {
+						name: '[name].[ext]',
+						outputPath: 'fonts',
+					},
+				},
+			},
+			{
+				test: /\.(webp|svg|png|jpe?g|gif)(\?\S*)?$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'imgs',
+						},
+					},
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							bypassOnDebug: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.(sa|sc|c)ss$/,
+				use: ['sass-loader'],
+			},
+			{
+				test: /\.(sa|sc|c)ss$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	devServer: {
+		historyApiFallback: true,
+	},
 
-    plugins: [
-        new CleanWebpackPlugin(),
+	plugins: [
+		new CleanWebpackPlugin(),
 
-        /** Webpack tool for analyzing the package size */
-        new BundleAnalyzerPlugin({
-            analyzerMode: 'static',
-            reportFilename: 'BundleReport.html',
-            logLevel: 'info',
-        }),
+		/** Webpack tool for analyzing the package size */
+		new BundleAnalyzerPlugin({
+			analyzerMode: 'disabled',
+		}),
 
-        new HtmlWebPackPlugin({
-            template: './client/index.html',
-            filename: './index.html',
-        }),
+		new HtmlWebPackPlugin({
+			template: './client/index.html',
+			filename: './index.html',
+		}),
 
-        /** Include the env parameters as web page parameters */
-        new webpack.DefinePlugin({
-            'process.env': envKeys,
-        }),
+		/** Include the env parameters as web page parameters */
+		new webpack.DefinePlugin({
+			'process.env': envKeys,
+		}),
 
-        /** Only introduce used moment locale package */
-        new webpack.ContextReplacementPlugin(/moment[\\]locale$/, /zh|en/),
+		/** Only introduce used moment locale package */
+		new webpack.ContextReplacementPlugin(/moment[\\]locale$/, /zh|en/),
 
-        /** Compression plugin */
-        new CompressionPlugin({
-            filename: '[path].br[query]',
-            algorithm: 'brotliCompress',
-            test: /\.(js)$/,
-            compressionOptions: {
-                level: 11,
-            },
-            // threshold: 10240,
-            minRatio: 0.8,
-            deleteOriginalAssets: false,
-        }),
+		/** Compression plugin */
+		new CompressionPlugin({
+			filename: '[path].br[query]',
+			algorithm: 'brotliCompress',
+			test: /\.(js)$/,
+			compressionOptions: {
+				level: 11,
+			},
+			// threshold: 10240,
+			minRatio: 0.8,
+			deleteOriginalAssets: false,
+		}),
 
-        new MiniCssExtractPlugin({
-            filename: './css/[name].css',
-        }),
+		new MiniCssExtractPlugin({
+			filename: './css/[name].css',
+		}),
 
-        new InjectManifest({
-            swSrc: path.join(
-                __dirname,
-                'client',
-                'js',
-                'serviceWorker',
-                'sw.js'
-            ),
-            swDest: path.join(__dirname, 'server/dist', 'sw.js'),
-            maximumFileSizeToCacheInBytes: 5000000000,
-        }),
+		new InjectManifest({
+			swSrc: path.join(__dirname, 'client', 'js', 'serviceWorker', 'sw.js'),
+			swDest: path.join(__dirname, 'server/dist', 'sw.js'),
+			maximumFileSizeToCacheInBytes: 5000000000,
+		}),
 
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'client/img/logo', to: 'imgs/logo' },
-                'client/manifest.webmanifest',
-            ],
-        }),
+		new CopyWebpackPlugin({
+			patterns: [
+				{ from: 'client/img/logo', to: 'imgs/logo' },
+				'client/manifest.webmanifest',
+			],
+		}),
 
-        new webpack.IgnorePlugin(/\.\/locale/, /moment/),
-    ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                parallel: true,
-            }),
-        ],
-        splitChunks: {
-            chunks: 'all',
-            cacheGroups: {
-                commons: {
-                    test: /[\\/]node_modules[\\/](!react-bootstrap)(!leaflet)(!mdbreact)(!xlsx)(!react-app-polyfill)[\\/]/,
-                    name: 'vendor',
-                },
-                xlsxVendor: {
-                    test: /[\\/]node_modules[\\/](xlsx)[\\/]/,
-                    name: 'xlsxVendor',
-                },
-                reactVendor: {
-                    test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-                    name: 'reactVendor',
-                },
-                leaflet: {
-                    test: /[\\/]node_modules[\\/](leaflet)[\\/]/,
-                    name: 'leafletVendor',
-                },
-                reactAppPolyfill: {
-                    test: /[\\/]node_modules[\\/](react-app-polyfill)[\\/]/,
-                    name: 'reactAppPolyfillVendor',
-                },
-                bootstrapVendor: {
-                    test: /[\\/]node_modules[\\/](react-bootstrap)[\\/]/,
-                    name: 'bootstrapVendor',
-                },
-            },
-        },
-    },
+		new webpack.IgnorePlugin(/\.\/locale/, /moment/),
+	],
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new TerserPlugin({
+				parallel: true,
+			}),
+		],
+		splitChunks: {
+			chunks: 'all',
+			cacheGroups: {
+				commons: {
+					test: /[\\/]node_modules[\\/](!react-bootstrap)(!leaflet)(!mdbreact)(!xlsx)(!react-app-polyfill)[\\/]/,
+					name: 'vendor',
+				},
+				xlsxVendor: {
+					test: /[\\/]node_modules[\\/](xlsx)[\\/]/,
+					name: 'xlsxVendor',
+				},
+				reactVendor: {
+					test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+					name: 'reactVendor',
+				},
+				leaflet: {
+					test: /[\\/]node_modules[\\/](leaflet)[\\/]/,
+					name: 'leafletVendor',
+				},
+				reactAppPolyfill: {
+					test: /[\\/]node_modules[\\/](react-app-polyfill)[\\/]/,
+					name: 'reactAppPolyfillVendor',
+				},
+				bootstrapVendor: {
+					test: /[\\/]node_modules[\\/](react-bootstrap)[\\/]/,
+					name: 'bootstrapVendor',
+				},
+			},
+		},
+	},
 }
