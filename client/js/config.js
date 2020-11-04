18f327161bca28cd26a8daf428f6528103afef0d
Johnson Jan Su <su1221jan@gmail.com>
2020-11-04 12:41:19 +0800

    build version up

--- client/js/config.js
+++ client/js/config.js
@@ -41,7 +41,7 @@ import moment from 'moment'
 import supportedLocale from './locale/supportedLocale'
 
 const config = {
-	VERSION: `v${version} b.1973`,
+	VERSION: `v${version} b.1974`,
 
 	TIMESTAMP_FORMAT: 'LLL',
 
