3dce5bbfc175c88f7baee14d0e8e4feced5f59b4
Johnson Jan Su <su1221jan@gmail.com>
2020-12-30 19:07:01 +0800

    update BOTTimePicker

--- client/js/components/BOTComponent/BOTTimePicker.js
+++ client/js/components/BOTComponent/BOTTimePicker.js
@@ -41,9 +41,7 @@ import 'rc-time-picker/assets/index.css'
 
 const covertToMoment = (value) => {
 	const [h, m, s] = value.split(':')
-	return moment()
-		.utcOffset(0)
-		.set({ hour: h, minute: m, second: s, millisecond: 0 })
+	return moment().set({ hour: h, minute: m, second: s, millisecond: 0 })
 }
 
 const BOTTimePicker = ({
