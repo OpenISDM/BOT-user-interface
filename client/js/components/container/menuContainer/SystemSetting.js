aa300992c6a645340a370157e4b7264c3f682d0e
Johnson Jan Su <su1221jan@gmail.com>
2021-01-04 10:56:43 +0800

    unlock scroll feature for Setting Pages

--- client/js/components/container/menuContainer/SystemSetting.js
+++ client/js/components/container/menuContainer/SystemSetting.js
@@ -55,14 +55,14 @@ class SystemSetting extends React.Component {
 	componentDidMount = () => {
 		/** set the scrollability in body disabled */
 		if (isMobileOnly || isTablet) {
-			const targetElement = document.querySelector('body')
-			enableBodyScroll(targetElement)
+			// const targetElement = document.querySelector('body')
+			// enableBodyScroll(targetElement)
 		}
 	}
 
 	componentWillUnmount = () => {
-		const targetElement = document.querySelector('body')
-		disableBodyScroll(targetElement)
+		// const targetElement = document.querySelector('body')
+		// disableBodyScroll(targetElement)
 	}
 
 	setMessage = (type, msg, isSetting) => {
