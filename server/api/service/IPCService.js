8e7ca7804456365f7729419a54a455616b74f781
Johnson Jan Su <su1221jan@gmail.com>
2021-01-11 15:50:15 +0800

    update IPCService and .env

--- server/api/service/IPCService.js
+++ server/api/service/IPCService.js
@@ -33,16 +33,29 @@
 */
 
 import 'dotenv/config'
-import child_process from 'child_process'
-const exec = child_process.execFile
+import { execFile } from 'child_process'
+
+const BOT_SERVER_IP = process.env.DB_HOST
 
 export const reloadGeofenceConfig = (area_id) => {
-	if (process.env.RELOAD_GEO_CONFIG_PATH) {
-		exec(
-			process.env.RELOAD_GEO_CONFIG_PATH,
-			`-p 9999 -c cmd_reload_geo_fence_setting -r geofence_object -f area_one -a ${area_id}`.split(
-				' '
-			),
+	if (process.env.IPC_EXECUTION_FILE) {
+		execFile(
+			process.env.IPC_EXECUTION_FILE,
+			[
+				'-s',
+				BOT_SERVER_IP,
+				'-p',
+				'9999',
+				'-c',
+				'cmd_reload_geo_fence_setting',
+				'-r',
+				'geofence_object',
+				'-f',
+				'area_one',
+				'-a',
+				area_id,
+			],
+			{ cwd: process.env.IPC_FOLDER_PATH },
 			function (err, data) {
 				if (err) {
 					console.log(`execute reload geofence setting failed ${err}`)
@@ -57,10 +70,21 @@ export const reloadGeofenceConfig = (area_id) => {
 }
 
 export const stopLightAlarm = ({ notificationId }) => {
-	if (process.env.IPC_EXECUTION_PATH) {
-		exec(
-			process.env.IPC_EXECUTION_PATH,
-			`-p 9999 -c cmd_stop_light_alarm -n ${notificationId}`.split(' '),
+	console.log(`stopLightAlarm notificationId:${notificationId}`)
+	if (process.env.IPC_EXECUTION_FILE) {
+		execFile(
+			process.env.IPC_EXECUTION_FILE,
+			[
+				'-s',
+				BOT_SERVER_IP,
+				'-p',
+				'9999',
+				'-c',
+				'cmd_stop_light_alarm',
+				'-n',
+				notificationId,
+			],
+			{ cwd: process.env.IPC_FOLDER_PATH },
 			function (err, data) {
 				if (err) {
 					console.log(`execute stop light alram failed ${err}`)
