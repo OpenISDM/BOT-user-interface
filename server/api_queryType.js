1b59613e1dd69abe64ae10481902fbe08024c1b2
Jason Chang <kenzz0979288190@gmail.com>
2021-01-15 16:15:09 +0800

    fix bug, object id error

--- server/api_queryType.js
+++ server/api_queryType.js
@@ -217,7 +217,7 @@ const getPeopleHistoryQuery = (
 
 const getPeopleRealtimeQuery = (key, filter) => {
 	return `select 
-	object_summary_table.id as object_id, 
+	object_table.id as object_id, 
 	object_summary_table.mac_address as mac_address, 
 	object_table.name as object_name, 
 	object_summary_table.updated_by_area as area_id, 
