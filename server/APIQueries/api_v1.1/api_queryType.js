9597ab9cc8eb14727def95ef8970be0c8c9143ac
Jason Chang <kenzz0979288190@gmail.com>
2021-01-22 14:58:53 +0800

    to remove old version querytype from new querytype document

--- server/APIQueries/api_v1.1/api_queryType.js
+++ server/APIQueries/api_v1.1/api_queryType.js
@@ -64,91 +64,6 @@ function setKey(user_id, username, hash) {
 const getAllKeyQuery = ' SELECT  * FROM api_key '
 const getAllUserQuery = ' SELECT  * FROM user_table	'
 
-const getPatientDurationQuery = (
-	key,
-	start_time,
-	end_time,
-	filter,
-	count_limit,
-	sort_type
-) => {
-	return `
-		WITH ranges AS (
-			SELECT object_id, mac_address, area_id, uuid, record_timestamp, battery_voltage, average_rssi, 
-				CASE WHEN LAG(uuid) OVER
-						(PARTITION BY mac_address
-							ORDER BY mac_address, record_timestamp) = uuid
-						THEN NULL ELSE 1 END r
-			FROM
-			(
-				SELECT
-					location_history_table.object_id as object_id,
-					location_history_table.mac_address AS mac_address,
-					location_history_table.area_id AS area_id,
-					location_history_table.uuid AS uuid,
-					location_history_table.record_timestamp AS record_timestamp,
-					location_history_table.battery_voltage AS battery_voltage,
-					location_history_table.average_rssi AS average_rssi
-				FROM location_history_table
-
-				INNER JOIN object_table
-				ON location_history_table.mac_address = object_table.mac_address
-					AND object_table.object_type != 0
-
-				INNER JOIN user_area
-				ON object_table.area_id = user_area.area_id
-
-				INNER JOIN user_table
-				ON user_table.id = user_area.user_id
-
-				
-
-		inner join api_key on
-		api_key.name = user_table.name
-		and api_key.key = '${key}'
-		where
-		record_timestamp > '${start_time}'
-		and record_timestamp < '${end_time}' 
-		${filter}
-		) as raw_data )
-				
-					, groups AS (
-						SELECT object_id, mac_address, area_id, uuid, record_timestamp, battery_voltage, average_rssi, r,
-							SUM(r)
-								OVER (ORDER BY mac_address, record_timestamp) grp
-						FROM ranges
-					)
-				
-					SELECT
-						MIN(groups.object_id::TEXT) as object_id,
-						MIN(groups.mac_address::TEXT) AS mac_address,
-						MIN(object_table.name) AS name,
-						MIN(groups.area_id) AS area_id,
-						MIN(area_table.readable_name) AS area_name,
-						MIN(groups.uuid::TEXT) AS uuid,
-						MIN(Lbeacon_table.description) AS beacon_description,
-						MIN(groups.battery_voltage) AS battery_voltage,
-						AVG(groups.average_rssi) AS avg_rssi,
-						MIN(groups.record_timestamp) AS start_time,
-						MAX(groups.record_timestamp) AS end_time,
-						MAX(groups.record_timestamp) - MIN(groups.record_timestamp)  AS duration
-					FROM groups
-				
-					INNER JOIN object_table
-					ON object_table.mac_address = groups.mac_address
-				
-					INNER JOIN area_table
-					ON area_table.id = groups.area_id
-				
-					INNER JOIN Lbeacon_table
-					ON Lbeacon_table.uuid = groups.uuid
-				
-					GROUP BY grp, groups.object_id
-					ORDER by mac_address ASC, start_time ${sort_type}
-					Limit ${count_limit}
-	`
-}
-
 const getPeopleHistoryQuery = (
 	key,
 	filter,
@@ -298,14 +213,6 @@ const getObjectIDFilter = (object_id) => {
 	return ''
 }
 
-const getLBeaconUUIDFilter = (uuids) => {
-	if (uuids) {
-		return `\nand location_history_table.uuid in  (${uuids.map(
-			(item) => `'${item}'`
-		)})`
-	}
-	return ''
-}
 
 const getObjectTypeFilter = (object_type) => {
 	if (object_type) {
@@ -392,7 +299,6 @@ export default {
 	setKey,
 	getAllKeyQuery,
 	getAllUserQuery,
-	getPatientDurationQuery,
 	getPeopleRealtimeQuery,
 	getPeopleHistoryQuery,
 	getObjectHistoryQuery,
@@ -402,6 +308,5 @@ export default {
 	getObjectTypeFilter,
 	getObjectTypeQuery,
 	getAreaIDQuery,
-	getLBeaconUUIDFilter,
 	getAreaIDFilter
 }
