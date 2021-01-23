d1a61d9f00bf940a6703fbe1fc6fe0c52b83d77a
Johnson Jan Su <su1221jan@gmail.com>
2021-01-23 13:19:19 +0800

    merge template.js to mailer.js

--- server/helper/mailer.js
+++ server/helper/mailer.js
@@ -34,6 +34,33 @@
 
 import nodemailer from 'nodemailer'
 import 'dotenv/config'
+const hostname = process.env.HOSTNAME
+const dataSrcProtocol = process.env.DATASRC_PROTOCOL || 'https'
+const domain = `${dataSrcProtocol}://${hostname}`
+
+export const resetPasswordInstruction = {
+	subject: 'BiDaE Object Tracker Password Assistance',
+
+	content: (token, accountNameList) => {
+		const hasMultipleAccount = accountNameList && accountNameList.length > 1
+		const botAccountText = hasMultipleAccount ? 'BOT accounts' : 'BOT account'
+
+		return `
+        Dear User,
+
+        Greetings from BiDaE Object Tracker Service.
+        We received a request to reset the password for the ${botAccountText} associated with this e-mail address.
+
+        Your ${botAccountText}: [${accountNameList.join(', ')}]
+
+        Click the link below to reset your password using our secure server:
+		${domain}/resetpassword/new/${token}
+
+        Sincerely,
+        The BiDaE Service Team
+        `
+	},
+}
 
 let config = {}
 if (process.env.EMAIL_HOST || process.env.EMAIL_PORT) {
