826dbf60cec0958c199e046f6abe183bdd97995a
Jason Chang <kenzz0979288190@gmail.com>
2021-02-22 15:17:15 +0800

    to enable area selection filter

--- client/js/domain/BatteryStatusTable.js
+++ client/js/domain/BatteryStatusTable.js
@@ -1,21 +1,26 @@
 import React from 'react'
+import { Row } from 'react-bootstrap'
 import { AppContext } from '../context/AppContext'
 import { batteryTableColumn } from '../config/tables'
 import { toast } from 'react-toastify'
 import { setSuccessMessage, setErrorMessage } from '../helper/messageGenerator'
 import API from '../api'
 import Table from '../components/Table'
+import ObjectFilterBar from '../components/ObjectFilterBar'
+import { SET_TABLE_SELECTION } from '../reducer/action'
 
 class BatteryStatusTable extends React.Component {
 	static contextType = AppContext
 
 	state = {
-		trackingData: [],
+		data: [],
 		tabIndex: 0,
 		locale: this.context.locale.abbr,
+		objectFilter: [],
+		filteredData: [],
+		filterSelection: [],
+		areaTable: [],
 	}
-
-	toastId = null
 
 	componentDidUpdate = (prevProps, prevState) => {
 		const { locale } = this.context
@@ -28,36 +33,65 @@ class BatteryStatusTable extends React.Component {
 		this.getTrackingData()
 	}
 
-	componentWillUnmount = () => {
-		toast.dismiss(this.toastId)
-	}
 
-	getTrackingData = async () => {
+	getTrackingData = async (callback) => {
 		const { locale, stateReducer } = this.context
 		const [{ area }] = stateReducer
-		const res = await API.Tracking.getTrackingData({
-			areaIds: [area.id],
-			locale: locale.abbr,
-		})
 
-		if (res) {
+		const BatteryDataPromise = API.Tracking.getTrackingData({
+			areaIds: [area.id],
+			locale: locale.addr,
+		})
+		const areaTablePromise = API.Area.getAreaTable()
+
+		const [BatteryDataRes, areaTableRes] = await Promise.all([
+			BatteryDataPromise,
+			areaTablePromise,
+		])
+
+		if (BatteryDataRes && areaTableRes) {
 			this.setMessage('clear')
-			const trackingData = res.data.map((item, index) => {
-				item.status = locale.texts[item.status.toUpperCase()]
-				item.transferred_location = ''
-				item._id = index + 1
+			const data = BatteryDataRes.data.map((item) => {
+				item.mac_address = item.mac_address
+					? item.mac_address
+					: locale.texts.NON_BINDING
+				item.area_name = {
+					value:item.lbeacon_area.value,
+					label:
+						item.lbeacon_area.value,
+					id: item.area_id,
+				}
 				return item
 			})
 
-			this.setState({
-				trackingData,
-				locale: locale.abbr,
+			const areaSelection = areaTableRes.data.map((area) => {
+				return {
+					value: area.name,
+					label: area.readable_name,
+				}
 			})
+			this.setState(
+				{
+					data,
+					filteredData: data,
+					areaTable: areaTableRes.data,
+					areaSelection,
+					locale: locale.abbr,
+				},
+				callback
+			)
+			this.clearSelection()
 		} else {
 			this.setMessage('error', 'connect to database failed', true)
 		}
 	}
-
+	clearSelection = () => {
+		const [, dispatch] = this.context.stateReducer
+		dispatch({
+			type: SET_TABLE_SELECTION,
+			value: [],
+		})
+	}
 	setMessage = (type, msg, isSetting) => {
 		switch (type) {
 			case 'success':
@@ -76,12 +110,48 @@ class BatteryStatusTable extends React.Component {
 	}
 
 	render() {
+		const { locale } = this.context
+		const filteredAttribute = [
+			'name',
+			'type',
+			'area',
+			'macAddress',
+			'acn',
+		]
 		return (
-			<Table
-				style={{ maxHeight: '85vh' }}
-				data={this.state.trackingData}
-				columns={batteryTableColumn}
-			/>
+			<>
+				<Row>
+					<ObjectFilterBar
+						onFilterUpdated={({ objectFilter, filteredData }) => {
+							this.setState({
+								objectFilter,
+								filteredData,
+							})
+							this.clearSelection()
+						}}
+						oldObjectFilter={this.state.objectFilter}
+						objectList={this.state.data}
+						selectionList={[
+							{
+								label: locale.texts.SEARCH,
+								attribute: filteredAttribute,
+								source: 'search bar',
+							},
+							{
+								label: locale.texts.AREA,
+								attribute: ['area'],
+								options: this.state.areaSelection,
+								source: 'area select',
+							},
+						]}
+					/>
+				</Row>
+				<Table
+					style={{ maxHeight: '85vh' }}
+					data={this.state.filteredData}
+					columns={batteryTableColumn}
+				/>
+			</>
 		)
 	}
 }
