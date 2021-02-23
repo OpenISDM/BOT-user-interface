622191951d8766f2303e4275b54a3473926e3900
Johnson Jan Su <su1221jan@gmail.com>
2021-02-23 11:11:38 +0800

    revert to correct path of webp

--- tools/webpCovertor.js
+++ tools/webpCovertor.js
@@ -10,7 +10,7 @@ const path = require('path')
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 const imagePath = path
-	.join(__dirname, '..', 'client', 'img', 'colorPin')
+	.join(__dirname, '..', 'server', 'public', 'map')
 	.replace(/\\/g, '/')
 
 ;(async () => {
