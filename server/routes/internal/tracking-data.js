aa4628fbdcb6acf94229170f3a4ba2f945bb77dd
Jason Chang <kenzz0979288190@gmail.com>
2021-03-08 18:14:57 +0800

    to add tracking-data route in backend

--- server/routes/internal/tracking-data.js
+++ server/routes/internal/tracking-data.js
@@ -2,4 +2,8 @@ import trackingController from '../../controllers/internal/tracking-data'
 
 export default (app) => {
 	app.route('/data/trackingData').post(trackingController.getTrackingData)
+
+	app
+		.route('/data/getTrackingTableByMacAddress')
+		.post(trackingController.getTrackingTableByMacAddress)
 }
