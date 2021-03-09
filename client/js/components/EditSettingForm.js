0584b4c6acc020fd8a9553b56eb1434280c43b15
Jason Chang <kenzz0979288190@gmail.com>
2021-03-09 16:25:18 +0800

    Revert "to save git record"
    This reverts commit 593dc278b5a581f4c5d10dc06b6894f6f1b84cdb.

---
+++ client/js/components/EditSettingForm.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import { Modal, Button } from 'react-bootstrap'
+import { AppContext } from '../context/AppContext'
+import { Formik, Form } from 'formik'
+import FormikFormGroup from '../domain/FormikFormGroup'
+import PropTypes from 'prop-types'
+
+const EditSettingForm = ({
+	title,
+	selectedObjectData = {},
+	show,
+	handleClose,
+	handleSubmit,
+	isShowDescription = true,
+	isShowUUID = true,
+}) => {
+	const { locale } = React.useContext(AppContext)
+	const { uuid, description, comment } = selectedObjectData
+	return (
+		<Modal
+			show={show}
+			onHide={handleClose}
+			size="md"
+			className="text-capitalize"
+		>
+			<Modal.Header closeButton>{title}</Modal.Header>
+			<Modal.Body>
+				<Formik
+					initialValues={{
+						description: description || '',
+						uuid,
+						comment,
+					}}
+					onSubmit={(values) => {
+						const { description, comment } = values
+						const settingPackage = {
+							...selectedObjectData,
+							description,
+							comment,
+						}
+						handleSubmit(settingPackage)
+					}}
+					render={({ errors, touched, isSubmitting }) => (
+						<Form>
+							{isShowUUID ? (
+								<FormikFormGroup
+									type="text"
+									name="uuid"
+									label={locale.texts.UUID}
+									error={errors.uuid}
+									touched={touched.uuid}
+									placeholder=""
+									disabled
+								/>
+							) : null}
+							{isShowDescription ? (
+								<FormikFormGroup
+									type="text"
+									name="description"
+									label={locale.texts.DESCRIPTION}
+									error={errors.description}
+									touched={touched.description}
+									placeholder=""
+								/>
+							) : null}
+							<FormikFormGroup
+								type="text"
+								name="comment"
+								label={locale.texts.COMMENT}
+								placeholder=""
+							/>
+							<Modal.Footer>
+								<Button variant="outline-secondary" onClick={handleClose}>
+									{locale.texts.CANCEL}
+								</Button>
+								<Button type="submit" variant="primary" disabled={isSubmitting}>
+									{locale.texts.SEND}
+								</Button>
+							</Modal.Footer>
+						</Form>
+					)}
+				/>
+			</Modal.Body>
+		</Modal>
+	)
+}
+
+EditSettingForm.propTypes = {
+	title: PropTypes.string,
+	selectedObjectData: PropTypes.object,
+	show: PropTypes.bool,
+	handleClose: PropTypes.func,
+	handleSubmit: PropTypes.func,
+	isShowUUID: PropTypes.bool,
+	isShowDescription: PropTypes.bool,
+}
+
+export default EditSettingForm
